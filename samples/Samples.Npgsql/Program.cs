7cf110656cc73c95a3780b61f0ffaf4ebb4fea9c
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-06-10 15:18:08 -0400

    add DbCommandWrapper to allow referencing DbCommand in netstandard.dll

--- samples/Samples.Npgsql/Program.cs
+++ samples/Samples.Npgsql/Program.cs
@@ -11,27 +11,9 @@ namespace Samples.Npgsql
     {
         private static async Task Main()
         {
-            /*
             using (var connection = CreateConnection())
             {
-                var testQueries = new RelationalDatabaseTestHarness<NpgsqlConnection, NpgsqlCommand, NpgsqlDataReader>(
-                    connection,
-                    command => command.ExecuteNonQuery(),
-                    command => command.ExecuteScalar(),
-                    command => command.ExecuteReader(),
-                    (command, behavior) => command.ExecuteReader(behavior),
-                    command => command.ExecuteNonQueryAsync(),
-                    command => command.ExecuteScalarAsync(),
-                    command => command.ExecuteReaderAsync(),
-                    (command, behavior) => command.ExecuteReaderAsync(behavior)
-                );
-
-                await testQueries.RunAsync();
-            }
-            */
-
-            using (var connection = CreateConnection())
-            {
+                // using DbDataReader here let's us run the ExecuteReaderAsync() overloads
                 var testQueries = new RelationalDatabaseTestHarness<NpgsqlConnection, NpgsqlCommand, DbDataReader>(
                     connection,
                     command => command.ExecuteNonQuery(),
@@ -46,6 +28,26 @@ namespace Samples.Npgsql
 
                 await testQueries.RunAsync();
             }
+
+#if NETCOREAPP
+            // use DbCommandWrapper to reference DbCommand in netstandard.dll
+            using (var connection = CreateConnection())
+            {
+                var testQueries = new RelationalDatabaseTestHarness<DbConnection, DbCommand, DbDataReader>(
+                    connection,
+                    command => new DbCommandWrapper(command).ExecuteNonQuery(),
+                    command => new DbCommandWrapper(command).ExecuteScalar(),
+                    command => new DbCommandWrapper(command).ExecuteReader(),
+                    (command, behavior) => new DbCommandWrapper(command).ExecuteReader(behavior),
+                    command => new DbCommandWrapper(command).ExecuteNonQueryAsync(),
+                    command => new DbCommandWrapper(command).ExecuteScalarAsync(),
+                    command => new DbCommandWrapper(command).ExecuteReaderAsync(),
+                    (command, behavior) => new DbCommandWrapper(command).ExecuteReaderAsync(behavior)
+                );
+
+                await testQueries.RunAsync("DbCommandWrapper");
+            }
+#endif
 
             using (var connection = CreateConnection())
             {
